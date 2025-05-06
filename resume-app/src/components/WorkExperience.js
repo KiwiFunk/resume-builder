2076927d4c628e4aa26a5fdbf0cfd98a190da96a
Alex <alex_p97@live.com>
2025-05-06 14:35:41 +0100

    Updated layout of work ex component for right aligned dates

--- resume-app/src/components/WorkExperience.js
+++ resume-app/src/components/WorkExperience.js
@@ -13,29 +13,34 @@ export default function WorkExperience({ data }) {
         <div className="mt-6 p-6 bg-white rounded shadow-lg w-full max-w-4xl">
             <h2 className="text-2xl font-bold text-gray-800">Experience</h2>
             {data.experience.map((job, index) => (
-                <div key={index} className="mt-4">
-                    <p className="text-gray-700 text-lg font-bold">{job.title}</p>
-                    <p className="text-gray-700">{job.company}</p>
-                    <p className="text-gray-700 text-sm">{job.location}</p>
+                <div key={index} className="mt-4 flex flex-row">
 
-                    {/* Format the start and end dates using toLocaleDateString */}
-                    <p className="text-gray-700 text-sm">
-                        {new Date(job.startDate).toLocaleDateString("en-US", {
-                        year: "numeric",
-                        month: "short",
-                        })}
-                        {" "}
-                        -
-                        {" "}
-                        {new Date(job.endDate).toLocaleDateString("en-US", {
-                        year: "numeric",
-                        month: "short",
-                        })}
-                    </p>
+                    <div className="flex-grow">
+                        <p className="text-gray-700 text-lg font-bold">{job.title}</p>
+                        <p className="text-gray-700">{job.company}</p>
+                        <p className="text-gray-700 text-sm">{job.location}</p>
 
-                    {formatJobDescription(job.description).map((point) => (
+                        {formatJobDescription(job.description).map((point) => (
                         <p className="text-gray-700 text-sm">{point}</p>
-                    ))}
+                        ))}
+                    </div>
+
+                    <div className="flex-shrink">
+                        {/* Format the start and end dates using toLocaleDateString */}
+                        <p className="text-gray-700 text-sm">
+                            {new Date(job.startDate).toLocaleDateString("en-US", {
+                            year: "numeric",
+                            month: "short",
+                            })}
+                            {" "}
+                            -
+                            {" "}
+                            {new Date(job.endDate).toLocaleDateString("en-US", {
+                            year: "numeric",
+                            month: "short",
+                            })}
+                        </p>
+                    </div>
 
                 </div>
             ))}
