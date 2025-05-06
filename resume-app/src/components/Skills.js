14e821ee439bd8dae53ad9933d49387a9d2ddd8c
Alex <alex_p97@live.com>
2025-05-06 15:26:27 +0100

    Created basic skills component

--- resume-app/src/components/Skills.js
+++ resume-app/src/components/Skills.js
@@ -1,22 +1,20 @@
 export default function Skills({ data }) {
+    // Expand to work with groups of skills in the future
+    // Iterate through groups, then through skills in each group
+    // User can create titled groups of skills. e.g. "Languages", "Frameworks", etc.
     return (
-        <div className="mt-4 p-6 bg-white rounded shadow-lg w-full max-w-4xl">
-            <h2 className="text-2xl font-bold text-gray-800">Skills</h2>
-            {data.skills.map((skill, index) => (
-                <div key={index} className="mt-2 flex flex-row items-center">
-                    <span className="rounded-full">{skill}</span>
-                </div>
-            ))}
-
-
-
-            <ul className="list-disc list-inside mt-4">
-                {data.skills.map((skill, index) => (
-                    <li key={index} className="text-gray-700">
-                        {skill}
-                    </li>
-                ))}
-            </ul>
+      <div className="mt-4 p-6 bg-white rounded-lg shadow-md w-full max-w-4xl">
+        <h2 className="text-2xl font-bold text-gray-800 mb-4">Skillset</h2>
+        <div className="flex flex-wrap gap-2">
+          {data.skills.map((skill, index) => (
+            <span
+              key={index}
+              className="px-4 py-2 rounded-full bg-(--accent) text-white text-sm font-semibold shadow-sm"
+            >
+              {skill}
+            </span>
+          ))}
         </div>
+      </div>
     );
-}
+  }
