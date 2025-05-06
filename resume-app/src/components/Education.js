4c79d1df4c2643150f7187cde09d5792b85e8ab0
Alex <alex_p97@live.com>
2025-05-06 14:32:43 +0100

    Updated layout of education component to display right aligned date

--- resume-app/src/components/Education.js
+++ resume-app/src/components/Education.js
@@ -3,25 +3,31 @@ export default function Education({ data }) {
         <div className="mt-6 p-6 bg-white rounded shadow-lg w-full max-w-4xl">
             <h2 className="text-2xl font-bold text-gray-800">Education</h2>
             {data.education.map((edu, index) => (
-                <div key={index} className="mt-4">
-                <p className="text-gray-700 text-lg font-bold">{edu.degree}</p>
-                <p className="text-gray-700">{edu.institution}</p>
-                <p className="text-gray-700 text-sm">{edu.location}</p>
+                <div key={index} className="mt-4 flex flex-row">
 
-                {/* Format the start and end dates using toLocaleDateString */}
-                <p className="text-gray-700 text-sm">
-                    {new Date(edu.startDate).toLocaleDateString("en-US", {
-                    year: "numeric",
-                    month: "short",
-                    })}
-                    {" "}
-                    -
-                    {" "}
-                    {new Date(edu.endDate).toLocaleDateString("en-US", {
-                    year: "numeric",
-                    month: "short",
-                    })}
-                </p>
+                    <div className="flex-grow">
+                        <p className="text-gray-700 text-lg font-bold">{edu.degree}</p>
+                        <p className="text-gray-700">{edu.institution}</p>
+                        <p className="text-gray-700 text-sm">{edu.location}</p>
+                    </div>
+
+                    {/* Format the start and end dates using toLocaleDateString */}
+                    <div className="flex-shrink">
+                        <p className="text-gray-700 text-sm">
+                            {new Date(edu.startDate).toLocaleDateString("en-US", {
+                            year: "numeric",
+                            month: "short",
+                            })}
+                            {" "}
+                            -
+                            {" "}
+                            {new Date(edu.endDate).toLocaleDateString("en-US", {
+                            year: "numeric",
+                            month: "short",
+                            })}
+                        </p>
+                    </div>  
+                                  
                 </div>
             ))}
         </div>
