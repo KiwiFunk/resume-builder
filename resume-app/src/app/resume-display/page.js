3dcb5d41f1b50089646a9ddb8da182fd79bf10b8
Alex <alex_p97@live.com>
2025-05-07 22:40:39 +0100

    Added routing to button onClick

--- resume-app/src/app/resume-display/page.js
+++ resume-app/src/app/resume-display/page.js
@@ -5,11 +5,16 @@ import DisplayResume from "./ResumeDisplay"; // Import the DisplayResume compone
 import data from "@/UserData"; // Import your user data
 
 export default function ResumeDisplayPage() {
+    const router = useRouter(); // Initialize the router
+
     return (
         <main className="flex flex-col items-center justify-center min-h-screen p-6">
             {/* Page navigation and tools such as save to PDF */}
             <div className="w-full max-w-4xl">
-                <button className="self-start"><i class="bi bi-chevron-left"></i></button>
+                <button 
+                    className="self-start cursor-pointer"
+                    onClick={() => router.push("/")} // Navigate back to the home pagemm
+                ><i className="bi bi-chevron-left"></i></button>
             </div>
 
             {/* Eventually, add a widget to handle selecting a resume template before displaying */}
