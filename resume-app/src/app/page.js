d90df97d9ca8886753a77a4e0545393fa5d74482
Alex <alex_p97@live.com>
2025-05-07 18:09:21 +0100

    removed temp footer

--- resume-app/src/app/page.js
+++ resume-app/src/app/page.js
@@ -36,10 +36,6 @@ export default function Home() {
       <WorkExperience data={data}/>
       <Hobbies data={data}/>
 
-      {/* Footer */}
-      <footer className="mt-4 p-6 bg-white rounded shadow-lg w-full max-w-4xl">
-        <p className="text-gray-700">© 2025 Resume Builder. All rights reserved.</p>
-      </footer>
     </main>
   );
 }
