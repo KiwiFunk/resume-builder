1116cbebe18704d9342e9590064a44d68c191682
Alex <alex_p97@live.com>
2025-05-08 12:00:20 +0100

    Added button for resume checking features

--- resume-app/src/app/resume-display/page.js
+++ resume-app/src/app/resume-display/page.js
@@ -18,12 +18,24 @@ export default function ResumeDisplayPage() {
                     <i className="bi bi-chevron-left text-xl"></i>
                 </button>
 
-                <button
-                    className="cursor-pointer hover:scale-110 transition-transform duration-200 ease-in-out"
-                    onClick={() => alert("PDF functionality coming soon!")}
-                >
-                    <i className="bi bi-file-earmark-pdf-fill text-xl"></i>
-                </button>
+                <div className="gap-4 flex flex-row items-center">
+
+                    {/* This feature will use scraping and simple JS to check for keyword matches against job listings, potentially expanded to AI suggestions */}
+                    <button
+                        className="cursor-pointer hover:scale-110 transition-transform duration-200 ease-in-out"
+                        onClick={() => alert("Check your resume for keyword matches against job listings, and ATS compatibility!")}
+                    >
+                        <i className="bi bi-check2-square text-xl"></i>
+                    </button>
+                    
+                    <button
+                        className="cursor-pointer hover:scale-110 transition-transform duration-200 ease-in-out"
+                        onClick={() => alert("PDF functionality coming soon!")}
+                    >
+                        <i className="bi bi-file-earmark-pdf-fill text-xl"></i>
+                    </button>
+                </div>
+                
                 </div>
 
             {/* Eventually, add a widget to handle selecting a resume template before displaying */}
