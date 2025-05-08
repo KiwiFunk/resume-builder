ed5d3b03f21477c2dd2df531820c12a0980460bc
Alex <alex_p97@live.com>
2025-05-08 20:34:01 +0100

    Added placeholder resume selector

--- resume-app/src/app/edit-details/page.js
+++ resume-app/src/app/edit-details/page.js
@@ -53,8 +53,29 @@ export default function EditDetailsPage() {
                     </div>
                 </div>
 
-
                 {/* Resume selector, with the option to create a new resume */}
+                <div className="mt-4 p-6 bg-white rounded shadow-lg w-full max-w-4xl flex gap-4">
+                    <span
+                        className="px-3 py-2 rounded-full bg-(--accent) text-white text-sm font-semibold shadow-sm"
+                    >
+                    Resume 1
+                    </span>
+                    <span
+                        className="px-3 py-2 rounded-full bg-(--accent) text-white text-sm font-semibold shadow-sm"
+                    >
+                    Resume 2
+                    </span>
+                    <span
+                        className="px-3 py-2 rounded-full bg-(--accent) text-white text-sm font-semibold shadow-sm"
+                    >
+                    Resume 3
+                    </span>
+                    <span
+                        className="px-3 py-2 rounded-full bg-(--accent) text-white text-sm font-semibold shadow-sm"
+                    >
+                    <i className="bi bi-plus"></i>
+                    </span>
+                </div>
 
                 {/* Resume Details */}
                 <div className="mt-4 p-6 bg-white rounded shadow-lg w-full max-w-4xl">
