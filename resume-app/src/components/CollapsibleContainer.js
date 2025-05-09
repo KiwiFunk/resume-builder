ed5cabd6bb21ed17eafe09db8ea4bc529c7fc979
Alex <alex_p97@live.com>
2025-05-09 12:40:36 +0100

    Added scrollIntoView on container state toggle

--- resume-app/src/components/CollapsibleContainer.js
+++ resume-app/src/components/CollapsibleContainer.js
@@ -1,13 +1,29 @@
 "use client"
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 export default function CollapsibleContainer({ title, children }) {
 
     // State to manage the visibility of the collapsible content
     const [isOpen, setIsOpen] = useState(true);
 
+    // Reference to this container
+    const containerRef = useRef(null);
+
+    // Toggle the collapsible content and scroll into view when expanded
+    const toggleCollapse = () => {
+        setIsOpen((prev) => {
+        if (!prev) {
+            // Scroll into view when expanding (Needs a timeout to work with opacity transition for some reason)
+            setTimeout(() => {
+                containerRef.current.scrollIntoView({ behavior: "smooth", block: "start" });
+            }, 20);
+        }
+        return !prev;
+        });
+    };
+
     return (
-        <div className="mt-4 p-6 bg-white rounded shadow-lg w-full max-w-4xl">
+        <div ref={containerRef} className="mt-4 p-6 bg-white rounded shadow-lg w-full max-w-4xl">
 
                     {/* Heading Bar */}
                     <div className="flex justify-between items-center">
@@ -16,15 +32,13 @@ export default function CollapsibleContainer({ title, children }) {
                         {/* Arrow icon */}
                         <i 
                             className="bi bi-caret-down-fill text-gray-300"
-                            onClick={() => setIsOpen(!isOpen)}  // Toggle the collapsible content
+                            onClick={toggleCollapse}  // Toggle the collapsible content
                         ></i>
                     </div>
 
                     {/* Collapsable content */}
                     <div
-                        className={`mt-2.5 transition-all duration-300 ease-in-out ${
-                        isOpen ? "max-h-screen opacity-100 pointer-events-auto" : "max-h-0 opacity-0 pointer-events-none"
-                        } overflow-hidden`}
+                        className={`mt-2.5 transition-all duration-100 ease-in-out ${isOpen ? "max-h-screen opacity-100" : "max-h-0 opacity-0 overflow-hidden"}`}
                     >
                         {children}
                         <p>TEST CONTENT</p>
