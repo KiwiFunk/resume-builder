db3116a34337b765c6f1684f552cbd79e0e6d7a3
Alex <alex_p97@live.com>
2025-05-10 16:47:54 +0100

    Created localData util file for functions handling localstorage data

---
+++ resume-app/src/utils/localData.js
@@ -0,0 +1,24 @@
+/**
+* @description Utility functions to manage local storage data.
+* @param {string} key - The key under which the data is stored in local storage.
+* @param {object} defaultValue - The default value to return if no data is found.
+* @returns {object} - The parsed data from local storage or the default value.
+*/
+
+// Fetch data from local storage by key
+export function getLocalData(key, defaultValue = {}) {
+    const data = localStorage.getItem(key);
+    // Check if data exists in local storage, if not return default value (empty object)
+    return data ? JSON.parse(data) : defaultValue;
+}
+
+// Save data to local storage
+export function setLocalData(key, data) {
+    localStorage.setItem(key, JSON.stringify(data));
+}
+
+// Delete data from local storage
+export function removeLocalData(key) {
+    // Clear the data stored under the specified key in local storage
+    localStorage.removeItem(key);
+}
