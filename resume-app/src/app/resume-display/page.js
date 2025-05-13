9c4029319c1fa8c0a87ea9edcb8e2fd30c328ffb
Alex <alex_p97@live.com>
2025-05-13 18:32:32 +0100

    Created A4 style to use for resume display

--- resume-app/src/app/resume-display/page.js
+++ resume-app/src/app/resume-display/page.js
@@ -164,7 +164,7 @@ export default function ResumeDisplayPage() {
               transformOrigin: 'top center'
             }}
           >
-            <div className="p-8">
+            <div className="p-8 a4-page">
               <DisplayResume data={data} template={template} />
             </div>
           </div>
