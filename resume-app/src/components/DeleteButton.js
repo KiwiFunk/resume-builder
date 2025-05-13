72bcdec71a25f0842e4b75dc6bb216ba36311e19
Alex <alex_p97@live.com>
2025-05-13 11:04:44 +0100

    Updated DeleteButton to properly use callback function

--- resume-app/src/components/DeleteButton.js
+++ resume-app/src/components/DeleteButton.js
@@ -1,9 +1,12 @@
 export default function DeleteButton({ onDelete }) {
     return (
-        <button 
-            type="button"
-            className="p-3 border border-gray-300 rounded text-red-500 hover:text-red-600 cursor-pointer hover:animate-wiggle hover:scale-110 transition-transform duration-100 ease-in-out"
-            onClick={() => handleDeletion("socials", index)}
+        <button
+            type="button"       // Prevents default 'submit' behavior
+            className="
+                p-3 border border-gray-300 rounded text-red-500 
+                hover:text-red-600 cursor-pointer hover:animate-wiggle 
+                hover:scale-110 transition-transform duration-100 ease-in-out"
+            onClick={onDelete}  // No need for extra arrow function wrapping as it is a reference to the function
         >
             <i className="bi bi-trash"></i>
         </button>
