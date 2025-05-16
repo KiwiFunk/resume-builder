b970a77edfa0038e8773070cf4f34809f9d8df25
Alex <alex_p97@live.com>
2025-05-16 19:01:32 +0100

    Added rect portal for loading component into iframe

--- resume-app/src/app/resume-display/DocumentViewer.js
+++ resume-app/src/app/resume-display/DocumentViewer.js
@@ -1,10 +1,31 @@
+"use client";
+import { useEffect, useState } from "react";
+import { createPortal } from "react-dom";
+
 export default function DocumentViewer({ children }) {
     
-    //Use Effect on load
+    const [ref, setRef] = useState(null);                   // State to hold the iframe reference
+    const container = ref?.contentWindow?.document?.body    // Get the body of the iframe document
+
+    useEffect(() => {
+        if (!iframeDocument) return;
+
+        const head = iframeDocument.head;
+
+        // Copy all stylesheets from the main document
+        document.querySelectorAll('link[rel="stylesheet"]').forEach(sheet => {
+            const clonedSheet = iframeDocument.createElement('link');
+            clonedSheet.rel = sheet.rel;
+            clonedSheet.href = sheet.href;
+            head.appendChild(clonedSheet);
+        });
+
+    }, [iframeDocument]);
     
     return (
-        <iframe>
-            { children }
+        <iframe ref={setRef}>
+            {/* Use a portal to render the children inside the iframe */}
+            { container && createPortal(children, container) }
         </iframe>
     );
 }
