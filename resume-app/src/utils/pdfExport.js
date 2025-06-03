c22f787914bb3baef6b508b07810477918ef813c
Alex <alex_p97@live.com>
2025-06-03 18:58:08 +0100

    Started creation of pdfExport util function

---
+++ resume-app/src/utils/pdfExport.js
@@ -0,0 +1,9 @@
+// Handle PDF export functionality
+/* Capture content of the iframe and convert it to a PDF */
+
+import html2canvas from 'html2canvas';
+import { jsPDF } from 'jspdf';
+
+export async function exportToPDF(iframeElement, userData) {
+
+}
